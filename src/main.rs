cc2bbe1095c9f473589a66669f2cd025fd6f7fc3
Andreas Kremsner <andreas.kremsner@senacor.com>
2020-11-05 09:40:21 +0100

    Read from Stdin
    + Call program by `echo -e "abc\ndef" | ./target/debug/rttr`
    + Call program by `echo "abc" | ./target/debug/rttr`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read};
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    let mut stdin = io::stdin();
+
+    stdin.read_to_string(&mut buffer)?;
+
+    println!("{}", buffer);
+
+    Ok(())
 }
