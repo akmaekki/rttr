c8cfcd876a4d7a56e6fde5f36ba63b13e91546af
Andreas Kremsner <andreas.kremsner@senacor.com>
2020-11-05 11:03:32 +0100

    Lagere Lesen von Stdin in Funktion aus

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 use std::io::{self, Read};
 
 fn main() -> io::Result<()> {
+    // Allocate buffer owned by main
     let mut buffer = String::new();
-    let mut stdin = io::stdin();
 
-    stdin.read_to_string(&mut buffer)?;
+    // Read from stdin into buffer
+    read_from_stdin(&mut buffer)?;
 
+    // Output read data from stdin
     println!("{}", buffer);
+
+    Ok(())
+}
+
+fn read_from_stdin(buffer: &mut String) -> io::Result<()>{
+    let mut stdin = io::stdin();
+    stdin.read_to_string(buffer)?;
 
     Ok(())
 }
